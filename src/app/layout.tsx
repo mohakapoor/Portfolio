609087be06d0ce92d5715b1015b67c93bfb45f41
mohakapoor <mohak.kapoor.v@gmail.com>
2026-03-22 22:35:46 +0530

    fixing viewpiort

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import {
   Bebas_Neue,
   Crimson_Text,
@@ -38,6 +38,11 @@ const courier = Courier_Prime({
   variable: "--font-courier",
   display: "swap",
 });
+
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+}
 
 export const metadata: Metadata = {
   title: "Mohak Kapoor | ML ENGINEER",
